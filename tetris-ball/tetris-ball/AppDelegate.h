2f8faa348e018c23ab5791e927a6aa5820f51a77
luizperes <luiz_peres10@hotmail.com>
2016-05-20 12:48:53 -0700

    First commit done

---
+++ tetris-ball/tetris-ball/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tetris-ball
+//
+//  Created by CICCC1 on 2016-05-20.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tetris-ball/tetris-ball/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  tetris-ball
+//
+
+//  Copyright (c) 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
