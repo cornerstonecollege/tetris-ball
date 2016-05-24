9f95a1e6f47febc911712e8753d703560224d926
luizperes <luiz_peres10@hotmail.com>
2016-05-24 09:41:56 -0700

    Added small changes to the sibling orders and background color

--- tetris-ball/tetris-ball/GameViewController.m
+++ tetris-ball/tetris-ball/GameViewController.m
@@ -17,14 +17,15 @@
 
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
+    skView.showsFPS = NO;
+    skView.showsNodeCount = NO;
     /* Sprite Kit applies additional optimizations to improve rendering performance */
-    skView.ignoresSiblingOrder = YES;
+    skView.ignoresSiblingOrder = NO;
     
     // Create and configure the scene.
     GameScene *scene = [GameScene nodeWithFileNamed:@"GameScene"];
     scene.scaleMode = SKSceneScaleModeAspectFill;
+    scene.backgroundColor = [SKColor whiteColor];
     
     // Present the scene.
     [skView presentScene:scene];
