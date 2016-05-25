5ce5e2e172042f96a7d36e474a18b68c869814a0
luizperes <luiz_peres10@hotmail.com>
2016-05-25 10:10:50 -0700

    Started to make the click events

--- tetris-ball/tetris-ball/GameScene.m
+++ tetris-ball/tetris-ball/GameScene.m
@@ -28,52 +28,11 @@
    __unused ShapeBackground *background = [[ShapeBackground alloc ] initWithColorLine:[SKColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:0.1] andParent:self];
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        static Ball *sprite;
-        if (!sprite)
-        {
-            sprite = [Ball ballDefaultWithParent:self];
-            sprite.position = location;
-            sprite.xScale = 0.1;
-            sprite.yScale = 0.1;
-            
-            SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-            [sprite runAction:[SKAction repeatActionForever:action]];
-        }
-        else
-        {
-            [sprite bounce];
-        }
-        
-        static Platform *platform;
-        
-        if (!platform)
-        {
-            matrixPlatform matrix = {0};
-            matrix[1][1] = 1;
-            matrix[2][0] = 1;
-            matrix[2][1] = 1;
-            matrix[2][2] = 1;
-            platform = [[Platform alloc] initWithMatrix:matrix andParent:self];
-            
-            
-            platform.xScale = 0.067;
-            platform.yScale = 0.067;
-            platform.position = location;
-            SKAction *action = [SKAction rotateByAngle:M_PI/2 duration:0.25];
-            [platform runAction:[SKAction repeatAction:action count:1]];
-        }
-        else
-        {
-            SKAction *action = [SKAction rotateByAngle:M_PI/2 duration:0.25];
-            [platform runAction:[SKAction repeatAction:action count:1]];
-        }
-    }
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+   __unused CGPoint location = [[touches anyObject] locationInNode:self];
 }
+
 
 - (void)update:(CFTimeInterval)currentTime
 {
